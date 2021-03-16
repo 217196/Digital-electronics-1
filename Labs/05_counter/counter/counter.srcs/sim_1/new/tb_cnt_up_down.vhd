38ad91d512838db93dce5bd5425f2f197921f803
Klára Heimlichová <xheiml02@vutbr.cz>
2021-03-16 22:13:12 +0100

    check

--- Labs/05_counter/counter/counter.srcs/sim_1/new/tb_cnt_up_down.vhd
+++ Labs/05_counter/counter/counter.srcs/sim_1/new/tb_cnt_up_down.vhd
@@ -1,9 +1,9 @@
 ------------------------------------------------------------------------
 --
--- Testbench for N-bit Up/Down binary counter.
+-- N-bit Up/Down binary counter.
 -- Nexys A7-50T, Vivado v2020.1.1, EDA Playground
 --
--- Copyright (c) 2020-Present Tomas Fryza
+-- Copyright (c) 2019-Present Tomas Fryza
 -- Dept. of Radio Electronics, Brno University of Technology, Czechia
 -- This work is licensed under the terms of the MIT license.
 --
@@ -11,96 +11,61 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 ------------------------------------------------------------------------
--- Entity declaration for testbench
+-- Entity declaration for n-bit counter
 ------------------------------------------------------------------------
-entity tb_cnt_up_down is
-    -- Entity of testbench is always empty
-end entity tb_cnt_up_down; 
+entity cnt_up_down is
+    generic(
+        g_CNT_WIDTH : natural := 4      -- Number of bits for counter
+    );
+    port(
+        clk      : in  std_logic;       -- Main clock
+        reset    : in  std_logic;       -- Synchronous reset
+        en_i     : in  std_logic;       -- Enable input
+        cnt_up_i : in  std_logic;       -- Direction of the counter
+        cnt_o    : out std_logic_vector(g_CNT_WIDTH - 1 downto 0)
+    );
+end entity cnt_up_down;
 
 ------------------------------------------------------------------------
--- Architecture body for testbench
+-- Architecture body for n-bit counter
 ------------------------------------------------------------------------
-architecture testbench of tb_cnt_up_down is
+architecture behavioral of cnt_up_down is
 
-    -- Number of bits for testbench counter
-    constant c_CNT_WIDTH         : natural := 5;
-    constant c_CLK_100MHZ_PERIOD : time    := 10 ns;
-
-    --Local signals
-    signal s_clk_100MHz : std_logic;
-    signal s_reset      : std_logic;
-    signal s_en         : std_logic;
-    signal s_cnt_up     : std_logic;
-    signal s_cnt        : std_logic_vector(c_CNT_WIDTH - 1 downto 0);
+    -- Local counter
+    signal s_cnt_local : unsigned(g_CNT_WIDTH - 1 downto 0);
 
 begin
-    -- Connecting testbench signals with cnt_up_down entity
-    -- (Unit Under Test)
-    uut_cnt : entity work.cnt_up_down
-        generic map(
-            g_CNT_WIDTH  => c_CNT_WIDTH
-        )
-        port map(
-            clk      => s_clk_100MHz,
-            reset    => s_reset,
-            en_i     => s_en,
-            cnt_up_i => s_cnt_up,
-            cnt_o    => s_cnt
-        );
-
     --------------------------------------------------------------------
-    -- Clock generation process
+    -- p_cnt_up_down:
+    -- Clocked process with synchronous reset which implements n-bit 
+    -- up/down counter.
     --------------------------------------------------------------------
-    p_clk_gen : process
+    p_cnt_up_down : process(clk)
     begin
-        while now < 750 ns loop         -- 75 periods of 100MHz clock
-            s_clk_100MHz <= '0';
-            wait for c_CLK_100MHZ_PERIOD / 2;
-            s_clk_100MHz <= '1';
-            wait for c_CLK_100MHZ_PERIOD / 2;
-        end loop;
-        wait;
-    end process p_clk_gen;
-
-    --------------------------------------------------------------------
-    -- Reset generation process
-    --------------------------------------------------------------------
-    p_reset_gen : process
-    begin
-        s_reset <= '0';
-        wait for 12 ns;
+        if rising_edge(clk) then
         
-        -- Reset activated
-        s_reset <= '1';
-        wait for 73 ns;
+            if (reset = '1') then               -- Synchronous reset
+                s_cnt_local <= (others => '0'); -- Clear all bits
 
-        s_reset <= '0';
-        wait;
-    end process p_reset_gen;
+            elsif (en_i = '1') then       -- Test if counter is enabled
 
-    --------------------------------------------------------------------
-    -- Data generation process
-    --------------------------------------------------------------------
-    p_stimulus : process
-    begin
-        report "Stimulus process started" severity note;
 
-        -- Enable counting
-        s_en     <= '1';
-        
-        -- Change counter direction
-        s_cnt_up <= '1';
-        wait for 380 ns;
-        s_cnt_up <= '0';
-        wait for 220 ns;
+                -- TEST COUNTER DIRECTION HERE
+                if (cnt_up_i = '1') then
+                    
+                    s_cnt_local <= s_cnt_local + 1;
+                else
+                    s_cnt_local <= s_cnt_local - 1;
+                end if;
 
-        -- Disable counting
-        s_en     <= '0';
+            end if;
+        end if;
+    end process p_cnt_up_down;
 
-        report "Stimulus process finished" severity note;
-        wait;
-    end process p_stimulus;
+    -- Output must be retyped from "unsigned" to "std_logic_vector"
+    cnt_o <= std_logic_vector(s_cnt_local);
 
-end architecture testbench;
+end architecture behavioral;
